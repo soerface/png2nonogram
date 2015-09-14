e98de56071babd6f3da10cc370402b5de0a94252
Soeren Wegener <wegener92@gmail.com>
2015-09-14 23:46:03 +0200

    Implemented drawing of fields.

--- nonogram.py
+++ nonogram.py
@@ -1,27 +1,36 @@
 import argparse
+import os
 import sys
 from PIL import Image, ImageDraw, ImageFont
 
-class Nonogram(object):
 
+class Nonogram(object):
     def __init__(self, filepath):
         img = Image.open(filepath).convert('RGB')
         self.col_number, self.row_number = img.size
+        self.data_matrix = [['' for y in range(self.row_number)] for x in range(self.col_number)]
+        print(len(self.data_matrix), len(self.data_matrix[0]))
         self.cols, self.rows = self.count_pixels(img)
         self.row_padding = max(map(len, self.cols))
         self.col_padding = max(map(len, self.rows))
+        self.is_solved = False
         self.image_padding = 5
         self.scale = 1
         self._pixel_size = 20
         self._font_size = 12
         self._font_padding_x = 2
         self._font_padding_y = 3
+        self._pixel_padding = 3
         self.font = 'DejaVuSans.ttf'
         self.background_color = '#161618'
         self.grid_bold_color = '#ee4d2e'
         self.grid_color = '#444'
         self.font_color = '#fff'
         self._grid_width = 1
+
+    @property
+    def pixel_padding(self):
+        return int(self._pixel_padding * self.scale)
 
     @property
     def pixel_size(self):
@@ -56,20 +65,63 @@ class Nonogram(object):
         out = Image.new('RGB', (width, height), self.background_color)
         self.draw_grid(out)
         self.draw_numbers(out)
+        self.draw_board(out)
         out.save(filepath)
+
+    def solve(self, directory):
+        try:
+            os.makedirs(directory)
+        except OSError, e:
+            if e.errno == 17:
+                # file / directory already exists
+                pass
+        i = 0
+        self.data_matrix[3][6] = 'X'
+        self.data_matrix[6][7] = '.'
+        while not self.is_solved:
+            self.save(os.path.join(directory, '{0}.png'.format(i)))
+            self.solve_step()
+            i += 1
+            break
+
+    def solve_step(self):
+        pass
+
+    def draw_board(self, img):
+        """Draws fields inside self.data_matrix. Used for solving the board"""
+        draw = ImageDraw.Draw(img)
+        for x, col in enumerate(self.data_matrix):
+            for y, value in enumerate(col):
+                x_pixel = x * self.pixel_size + self.col_padding * self.pixel_size + self.image_padding
+                y_pixel = y * self.pixel_size + self.row_padding * self.pixel_size + self.image_padding
+                if value == 'X':
+                    padding = self.pixel_padding
+                    coordinates = (x_pixel + padding, y_pixel + padding,
+                                   x_pixel + self.pixel_size - padding,
+                                   y_pixel + self.pixel_size - padding)
+                    draw.rectangle(coordinates, fill=self.font_color)
+                elif value == '.':
+                    padding = 0
+                    coordinates = (x_pixel + padding, y_pixel + padding,
+                                   x_pixel + self.pixel_size - padding,
+                                   y_pixel + self.pixel_size - padding)
+                    print coordinates
+                    draw.line(coordinates, fill=self.font_color)
+                    coordinates = (coordinates[0], coordinates[3], coordinates[1], coordinates[2])
+                    draw.line(coordinates, fill=self.font_color)
 
     def draw_numbers(self, img):
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype(self.font, size=self.font_size)
         x = self.col_padding * self.pixel_size + self.image_padding + self.font_padding_x
         for col in self.cols:
-            y = (self.row_padding - 1 ) * self.pixel_size + self.image_padding + self.font_padding_y
+            y = (self.row_padding - 1) * self.pixel_size + self.image_padding + self.font_padding_y
             for value in col[::-1]:
                 if value < 10:
                     additional_space = self.font_size / 3
                 else:
                     additional_space = 0
-                draw.text((x+additional_space, y), str(value), font=font, fill=self.font_color)
+                draw.text((x + additional_space, y), str(value), font=font, fill=self.font_color)
                 y -= self.pixel_size
             x += self.pixel_size
         y = self.row_padding * self.pixel_size + self.image_padding + self.font_padding_y
@@ -80,7 +132,7 @@ class Nonogram(object):
                     additional_space = self.font_size / 3
                 else:
                     additional_space = 0
-                draw.text((x+additional_space, y), str(value), font=font, fill=self.font_color)
+                draw.text((x + additional_space, y), str(value), font=font, fill=self.font_color)
                 x -= self.pixel_size
             y += self.pixel_size
 
@@ -106,11 +158,13 @@ class Nonogram(object):
                 draw.line((padding, y, width - self.image_padding, y), fill=self.grid_color, width=self.grid_width)
             row_counter += 1
         for x in range(self.image_padding + self.col_padding * self.pixel_size, width, self.pixel_size * 5):
-            draw.line((x, self.image_padding, x, height - self.image_padding), fill=self.grid_bold_color, width=self.grid_bold_width)
+            draw.line((x, self.image_padding, x, height - self.image_padding), fill=self.grid_bold_color,
+                      width=self.grid_bold_width)
         # draw.line((width - self.image_padding, self.image_padding, width - self.image_padding, height - self.image_padding), fill=self.grid_bold_color, width=self.grid_bold_width)
         for y in range(self.image_padding + self.row_padding * self.pixel_size, height, self.pixel_size * 5):
-            draw.line((self.image_padding, y, width - self.image_padding, y), fill=self.grid_bold_color, width=self.grid_bold_width)
-        # draw.line((self.image_padding, height - self.image_padding, width - self.image_padding, height - self.image_padding), fill=self.grid_bold_color, width=self.grid_bold_width)
+            draw.line((self.image_padding, y, width - self.image_padding, y), fill=self.grid_bold_color,
+                      width=self.grid_bold_width)
+            # draw.line((self.image_padding, height - self.image_padding, width - self.image_padding, height - self.image_padding), fill=self.grid_bold_color, width=self.grid_bold_width)
 
     def count_pixels(self, img):
         width, height = img.size
@@ -148,6 +202,7 @@ class Nonogram(object):
         r, g, b = img.getpixel((x, y))
         return False if (r + g + b) / 3 > 127 else True
 
+
 def hexcolor(value):
     if value.startswith('#'):
         value = value[1:]
@@ -159,6 +214,7 @@ def hexcolor(value):
         raise ValueError('Invalid color, must be exactly 3 or 6 characters long')
     return '#{0}'.format(value)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filepath', help='Path to the image that should be nonofied')
@@ -168,6 +224,8 @@ if __name__ == '__main__':
     parser.add_argument('-fc', '--font-color', default='#000', help='Font color', type=hexcolor)
     parser.add_argument('-gc', '--grid-color', default='#888', help='Grid color', type=hexcolor)
     parser.add_argument('-gbc', '--grid-bold-color', default='#000', help='Grid bold color', type=hexcolor)
+    parser.add_argument('-pp', '--pixel-padding', default=3, help='Scaling of the output image', type=float)
+    parser.add_argument('--solve', action='store_true', help='Generate images which solve the puzzle')
     args = parser.parse_args()
     try:
         nonogram = Nonogram(args.filepath)
@@ -178,4 +236,8 @@ if __name__ == '__main__':
     nonogram.grid_bold_color = args.grid_bold_color
     nonogram.grid_color = args.grid_color
     nonogram.font_color = args.font_color
-    nonogram.save(args.output)
+    nonogram._pixel_padding = args.pixel_padding
+    if args.solve:
+        nonogram.solve(args.output)
+    else:
+        nonogram.save(args.output)
