788ba68f8031dee482eaeb9b668ddb3134efb7ad
Soeren Wegener <wegener92@gmail.com>
2016-12-04 04:48:40 +0100

    Playing around with pycosat.

--- solver.py
+++ solver.py
@@ -2,10 +2,14 @@
 from typing import List
 import numpy as np
 from random import random
+import pycosat
 
 def solve(rows : List, cols : List, data : np.ndarray) -> bool:
     print(rows)
     print(cols)
     print(data.shape)
-    for i in range(int(random() * data.shape[0] * data.shape[1])):
-        data[random() * data.shape[0], random() * data.shape[1]] = 1 if random() > 0.5 else -1
+    # for i in range(int(random() * data.shape[0] * data.shape[1])):
+    #     data[random() * data.shape[0], random() * data.shape[1]] = 1 if random() > 0.5 else -1
+    cnf = [[1], [1, 3], [2, 3]]
+    for solution in pycosat.itersolve(cnf):
+        print(solution)
